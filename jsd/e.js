aff2f323dcae91faa38e9d3be11fbc619fd70fbb
joaovictor307 <163419377+joaovictor307@users.noreply.github.com>
2024-07-30 12:48:24 -0300

    Add files via upload

---
+++ jsd/e.js
@@ -0,0 +1,12 @@
+let vogal=[10]
+
+ for (let cont = 0; cont <10; cont++) {
+    vogal[cont]=prompt("informe a vogal: ")
+    
+ }for (let i = 0; i<10; i++) {
+  if (vogal[i]=="a"|| vogal[i]=="A") {
+   a=a++
+  }
+  if(vogal[i]=="e"||"E")
+    e=e++
+ }
