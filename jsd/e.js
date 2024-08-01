02b6be5b6b413613437beaaf1313e862104fa689
joaovictor307 <163419377+joaovictor307@users.noreply.github.com>
2024-08-01 09:53:05 -0300

    Add files via upload

--- jsd/e.js
+++ jsd/e.js
@@ -1,12 +1,36 @@
-let vogal=[10]
+let vogal = [10]
+let a=0;
+let i=0
+let o=0
+let u =0
+let e=0
 
- for (let cont = 0; cont <10; cont++) {
-    vogal[cont]=prompt("informe a vogal: ")
-    
- }for (let i = 0; i<10; i++) {
-  if (vogal[i]=="a"|| vogal[i]=="A") {
-   a=a++
-  }
-  if(vogal[i]=="e"||"E")
-    e=e++
- }
+for (let cont = 0; cont < 10; cont++) {
+   vogal[cont] = prompt("informe a vogal: ")
+
+}
+
+for (let f = 0; f < 10; f++) {
+   if (vogal[f] == "a" || vogal[f] == "A") {
+    a = a+1
+   }
+   if (vogal[f] == "e" || vogal[f] == "E") {
+    e = e+1
+   } if (vogal[f] == "i" || vogal[f] == "I") {
+      i = i+1
+   } if (vogal[f] == "o" || vogal[f] == "O") {
+      o = o++
+   } if (vogal[f] == "u" || vogal[f] == "U") {
+      u = u+1
+   }
+}
+
+document.write(`a qauntidade da vogal a é = ${a} `)
+document.write("<br>")
+document.write(`a qauntidade da vogal E é igual a = ${e} `)
+document.write("<br>")
+document.write(`a qauntidade da vogal I é igual a = ${i} `)
+document.write("<br>")
+document.write(`a qauntidade da vogal O é igual a = ${o} `)
+document.write("<br>")
+document.write(`a qauntidade da vogal U é igual a = ${u} `)
